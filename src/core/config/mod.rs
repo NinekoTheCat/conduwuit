99f6f40233f581d3e9d04b0c545b5d2311eb12b9
Niko <cnotsomark@gmail.com>
2025-02-01 19:08:56 -0500

    Added configuration for blurhashing

--- src/core/config/mod.rs
+++ src/core/config/mod.rs
@@ -1758,6 +1758,8 @@ pub struct Config {
 	#[serde(default = "true_fn")]
 	pub config_reload_signal: bool,
 
+	// has its own section
+	pub blurhashing_config: BlurhashConfig,
 	#[serde(flatten)]
 	#[allow(clippy::zero_sized_map_values)]
 	// this is a catchall, the map shouldn't be zero at runtime
@@ -1806,6 +1808,30 @@ pub struct WellKnownConfig {
 	pub support_email: Option<String>,
 
 	pub support_mxid: Option<OwnedUserId>,
+}
+#[cfg(feature="blurhashing")]
+#[derive(Clone,Copy, Debug, Deserialize, Default)]
+#[allow(rustdoc::broken_intra_doc_links, rustdoc::bare_urls)]
+#[config_example_generator(filename = "conduwuit-example.toml", section = "global.blurhashing")]
+pub struct BlurhashConfig {
+	/// blurhashing y component, 4 is recommended by https://blurha.sh/
+	/// 
+	/// default: 4
+	#[serde(default = "default_blurhash_x_component")]
+	pub components_x: u32,
+	/// blurhashing y component, 3 is and recommended by https://blurha.sh/
+	/// 
+	/// default: 3
+	#[serde(default = "default_blurhash_y_component")]
+	pub components_y: u32,
+	/// Max raw size that the server will blurhash, this is the size of the image after converting it to raw data, 
+	/// it should be higher than the upload limit but not too high. 
+	/// The higher it is the higher the potential load will be for clients requesting blurhashes.
+	/// The default is 33.55MB.
+	/// 
+	/// default: 33554432
+	#[serde(default = "default_blurhash_max_raw_size")]
+	pub blurhash_max_raw_size: u64
 }
 
 #[derive(Deserialize, Clone, Debug)]
@@ -2169,3 +2195,16 @@ fn default_stream_width_default() -> usize { 32 }
 fn default_stream_width_scale() -> f32 { 1.0 }
 
 fn default_stream_amplification() -> usize { 1024 }
+
+// blurhashing defaults recommended by https://blurha.sh/
+// 2^25
+#[cfg(feature = "blurhashing")]
+pub(super) fn default_blurhash_max_raw_size() -> u64 {33554432}
+
+#[cfg(feature = "blurhashing")]
+pub(super) fn default_blurhash_x_component() -> u32 {4}	
+
+#[cfg(feature = "blurhashing")]
+pub(super) fn default_blurhash_y_component() -> u32 {3}		
+
+// end recommended & blurhashing defaults
